6baf93cdc83019e0569e1bb0b90917386d580197
giuseppezaccato <giuseppezaccato@gmail.com>
2025-02-06 18:04:41 +0100

    END: fine esercizio

--- people.js
+++ people.js
@@ -7,25 +7,27 @@ const { freeTime } = require("./hobbies.js");
 
 function newObj() {
 
-    //task tentativo fallito chiamata api :(
-    let nome;
-    let cognome;
+    //task tentativo fallito chiamata api mi ritorna undefined 
+    //task anche se metto l'oggetto DENTRO al .then(data=> ) 
+    //task però il console log il nome e il cognome me lo recuperano! :(
 
-    fetch('https://randomuser.me/api/', { method: "GET" })
-        .then(response => response.json())
-        .then(data => {
-            nome = data.results[0].name.first;
-            cognome = data.results[0].name.last;
-            console.log(nome, cognome); // Stampa sia nome che cognome
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    // let firstName;
+    // let lastName;
 
+    // fetch('https://randomuser.me/api/', { method: "GET" })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         const nome = data.results[0].name.first;
+    //         const cognome = data.results[0].name.last;
+    //         console.log(nome, cognome); // Stampa sia nome che cognome
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     });
 
     return {
         //* alla chiave FullName associo il valore che equivale all'evocazione della funzione person()
-        FullName: person(nome, cognome),
+        FullName: person("Giuseppe", "Zaccato"),
 
         //* alla chiave TempoLibero assegno come valore l'evocazione di freeTime(che automaticamente estrae un random tra quelli aggiunti)
         TempoLibero: freeTime("tennis", "coding", "football", "crosswords", "reading", "running")
